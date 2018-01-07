acccea6f927229dda855ffe4a720612448043f7f
Jhovie Anggoro <jhovie.anggoro@gmail.com>
2018-01-07 21:30:01 +0700

    commit #2

---
+++ src/main/java/com/test/hc/PermutationCheckRotation.java
@@ -0,0 +1,77 @@
+package com.test.hc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PermutationCheckRotation {
+	public static void main(String[] args) {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(System.in);
+			
+			// initialize array
+			System.out.print("Length: ");
+			int arrLength = sc.nextInt();
+			System.out.println();
+			int[] oriArr = new int[arrLength];
+			for (int i = 0; i < arrLength; i++) {
+				System.out.print("Add value: ");
+				oriArr[i] = sc.nextInt();
+				System.out.println();
+			}
+			System.out.println("Array created: " + Arrays.toString(oriArr));
+			
+			// shift 
+			System.out.print("Shifting Right: ");
+			int shift = sc.nextInt();
+			System.out.println();
+			
+			shift = shift % arrLength;
+			if (shift > 0) {
+				int[] newArr = new int[arrLength];
+				for (int i = 0; i < arrLength; i++) {
+					int newIdx = i + shift;
+					if (newIdx > (arrLength - 1)) {
+						newIdx = newIdx - arrLength;
+					}
+					newArr[newIdx] = oriArr[i];
+				}
+				System.out.println("New Array: " + Arrays.toString(newArr));
+			} else {
+				System.out.println("New Array: " + Arrays.toString(oriArr));
+			}
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+	
+	public int solution(int[] a) {
+		if (a.length > 0) {
+			// assigning to temp
+			int[] tempArr = new int[a.length];
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] > a.length || a[i] < 1) {
+					return 0;
+				} else {
+					tempArr[a[i] - 1] = a[i];
+				}
+			}
+			
+			// checking temp array
+			for (int i = 0; i < tempArr.length; i++) {
+				if (tempArr[i] == 0) {
+					return 0;
+				}
+			}
+			return 1;
+		}
+		return 0;
+	}
+}
